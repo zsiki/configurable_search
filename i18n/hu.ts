ce2462a8f6dd11e3c559ca9e50fb24bf0df0e498
zsiki <siki1958@gmail.com>
2020-02-01 21:08:34 +0100

    initial commit

---
+++ i18n/hu.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Jó reggelt</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigurableSearch</name>
+    <message>
+        <location filename="../configurable_search.py" line="191"/>
+        <source>&amp;Configurable search</source>
+        <translation>Konfigurálható keresés</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search.py" line="75"/>
+        <source>Missing file</source>
+        <translation>Hiányzó fájl</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search.py" line="75"/>
+        <source>Config file not found: {}</source>
+        <translation>A konfigurációs fájl nem találom: {}</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search.py" line="81"/>
+        <source>Config file is not valid: {}</source>
+        <translation>Hibás konfiguráció fájl: {}</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search.py" line="182"/>
+        <source>Search</source>
+        <translation>Keresés</translation>
+    </message>
+</context>
+<context>
+    <name>LayerSearchDialog</name>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="14"/>
+        <source>Search Layers</source>
+        <translation>Keresés</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="29"/>
+        <source>Search String</source>
+        <translation>Keresett szöveg</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="39"/>
+        <source>Search for</source>
+        <translation>Keresés típus</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="49"/>
+        <source>Comparison</source>
+        <translation>Összehasonlítás</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="90"/>
+        <source>Search</source>
+        <translation>Keres</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="97"/>
+        <source>Abort</source>
+        <translation>Megszakít</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="104"/>
+        <source>Clear</source>
+        <translation>Töröl</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog_base.ui" line="111"/>
+        <source>Exit</source>
+        <translation>Kilép</translation>
+    </message>
+</context>
+<context>
+    <name>plugin</name>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="52"/>
+        <source>Value</source>
+        <translation>Érték</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="52"/>
+        <source>Layer</source>
+        <translation>Réteg</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="52"/>
+        <source>Field</source>
+        <translation>Mező</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="52"/>
+        <source>Feature Id</source>
+        <translation>Elem ID</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="55"/>
+        <source>=</source>
+        <translation>Teljes egyezés</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="55"/>
+        <source>contains</source>
+        <translation>Tartalmazza</translation>
+    </message>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="55"/>
+        <source>begins with</source>
+        <translation>Ezzel kezdődik</translation>
+    </message>
+</context>
+<context>
+    <name>self.plugin</name>
+    <message>
+        <location filename="../configurable_search_dialog.py" line="104"/>
+        <source>There are no vector layers to search through</source>
+        <translation>Nincs keresendő vektor réteg</translation>
+    </message>
+</context>
+</TS>
